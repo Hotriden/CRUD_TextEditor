9696cbe81016968c0cf3ba97acd16d413994d522
Hotriden <bloodridetv@gmail.com>
2018-05-22 11:19:48 +0300

    Add project

---
+++ CRUD_App/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace CRUD_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblField = new System.Windows.Forms.Label();
+            this.fldPath = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.numFont = new System.Windows.Forms.NumericUpDown();
+            this.lblFont = new System.Windows.Forms.Label();
+            this.fldContent = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblSymbolCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblCounter = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.numFont)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblField
+            // 
+            this.lblField.AutoSize = true;
+            this.lblField.Location = new System.Drawing.Point(12, 13);
+            this.lblField.Name = "lblField";
+            this.lblField.Size = new System.Drawing.Size(53, 13);
+            this.lblField.TabIndex = 0;
+            this.lblField.Text = "Chose file";
+            // 
+            // fldPath
+            // 
+            this.fldPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fldPath.Location = new System.Drawing.Point(71, 10);
+            this.fldPath.Name = "fldPath";
+            this.fldPath.Size = new System.Drawing.Size(351, 20);
+            this.fldPath.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen.Location = new System.Drawing.Point(509, 10);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelect.Location = new System.Drawing.Point(428, 10);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 3;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            // 
+            // numFont
+            // 
+            this.numFont.Location = new System.Drawing.Point(71, 47);
+            this.numFont.Maximum = new decimal(new int[] {
+            72,
+            0,
+            0,
+            0});
+            this.numFont.Minimum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numFont.Name = "numFont";
+            this.numFont.Size = new System.Drawing.Size(37, 20);
+            this.numFont.TabIndex = 4;
+            this.numFont.Value = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            // 
+            // lblFont
+            // 
+            this.lblFont.AutoSize = true;
+            this.lblFont.Location = new System.Drawing.Point(12, 49);
+            this.lblFont.Name = "lblFont";
+            this.lblFont.Size = new System.Drawing.Size(48, 13);
+            this.lblFont.TabIndex = 5;
+            this.lblFont.Text = "FontSize";
+            // 
+            // fldContent
+            // 
+            this.fldContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fldContent.Location = new System.Drawing.Point(15, 73);
+            this.fldContent.Multiline = true;
+            this.fldContent.Name = "fldContent";
+            this.fldContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.fldContent.Size = new System.Drawing.Size(564, 276);
+            this.fldContent.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(504, 356);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblSymbolCount,
+            this.lblCounter});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 382);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(591, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblSymbolCount
+            // 
+            this.lblSymbolCount.Name = "lblSymbolCount";
+            this.lblSymbolCount.Size = new System.Drawing.Size(63, 17);
+            this.lblSymbolCount.Text = "Characters";
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 404);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.fldContent);
+            this.Controls.Add(this.lblFont);
+            this.Controls.Add(this.numFont);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.fldPath);
+            this.Controls.Add(this.lblField);
+            this.Name = "Form1";
+            this.Text = "TextEditor";
+            ((System.ComponentModel.ISupportInitialize)(this.numFont)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblField;
+        private System.Windows.Forms.TextBox fldPath;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.NumericUpDown numFont;
+        private System.Windows.Forms.Label lblFont;
+        private System.Windows.Forms.TextBox fldContent;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblSymbolCount;
+        private System.Windows.Forms.ToolStripStatusLabel lblCounter;
+    }
+}
+
